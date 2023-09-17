f03fa370e769e342bf738add869e7653623b02e5
OctavianoSoto <osoto.iv@gmail.com>
2023-09-17 09:57:30 -0500

    npm nvm update

--- src/configs.js
+++ src/configs.js
@@ -2,9 +2,6 @@
 const BASE_API_URL = process.env.NODE_ENV === "development" ? "http://localhost:3001" : "https://stellar-api-4c4337d94ef4.herokuapp.com"
 const SECRET_USER = process.env.REACT_APP_SECRET_USER;
 const PW_SECRET = process.env.REACT_APP_PW_SECRET
-console.log(BASE_API_URL)
-console.log(SECRET_USER)
-console.log(PW_SECRET)
 export {
     BASE_API_URL,
     SECRET_USER,
