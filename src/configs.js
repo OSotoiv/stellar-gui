b150f3c3013a567cf9284157fba005ab3f64115b
OctavianoSoto <osoto.iv@gmail.com>
2023-09-17 06:33:29 -0500

    NODE_ENV added

--- src/configs.js
+++ src/configs.js
@@ -1,5 +1,5 @@
 
-const BASE_API_URL = process.env.REACT_APP_ENV === "development" ? "http://localhost:3001" : "https://stellar-api-4c4337d94ef4.herokuapp.com"
+const BASE_API_URL = process.env.NODE_ENV === "development" ? "http://localhost:3001" : "https://stellar-api-4c4337d94ef4.herokuapp.com"
 const SECRET_USER = process.env.REACT_APP_SECRET_USER;
 const PW_SECRET = process.env.REACT_APP_PW_SECRET
 
