40c1938e1493dcf83c0c4290a0c77195d8ede435
OctavianoSoto <osoto.iv@gmail.com>
2023-09-17 11:12:31 -0500

    removed webvitals

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,4 +14,3 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
